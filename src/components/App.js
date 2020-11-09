b9f7e4e5ea1fe6f6c676e8cab76f0abf6f3f15cb
lukabajic <lukabajic.dev@gmail.com>
2020-11-09 21:15:20 +0100

    Add Register and Login routes/pages

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,24 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 
 import Index from "./Pages/Index/Index";
+import Register from "./Pages/Register/Register";
+import Login from "./Pages/Login/Login";
 
 const App = () => {
-  return <Index />;
+  return (
+    <Switch>
+      <Route path="/auth/register" exact>
+        <Register />
+      </Route>
+      <Route path="/auth/login">
+        <Login />
+      </Route>
+      <Route path="/">
+        <Index />
+      </Route>
+    </Switch>
+  );
 };
 
 export default App;
