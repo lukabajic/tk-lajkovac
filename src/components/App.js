d93e365f16e84c787fc0d868a46d6a5c024e5127
lukabajic <lukabajic.dev@gmail.com>
2020-11-09 20:56:02 +0100

    Add React Router and <Button />
    Installed react-router-dom packege. Wrapped the <App /> in index.js
    so <Link /> in the Button component doesn't throw an error. Routing
    up next.
    
    Added a reusable button component. A couple of props can be passed
    to it to change styling or function.

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import * as styles from "./App.module.css";
 
 import Logo from "./Utility/Logo/Logo";
+import Button from "./Utility/Button/Button";
 
 const App = () => {
   return (
@@ -18,6 +19,14 @@ const App = () => {
         aplikacija teniskog kluba Lajkovac, uz pomoć koje možete pronaći kontakt
         drugih članova i zakazati meč.
       </p>
+      <div className={styles.buttons}>
+        <Button fluid primary href="/auth/login">
+          Ulogujte se
+        </Button>
+        <Button fluid secondary href="/auth/register">
+          Registrujte se
+        </Button>
+      </div>
     </main>
   );
 };
---
+++ src/components/Utility/Button/Button.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import * as styles from "./Button.module.css";
+
+const Button = (props) => {
+  const buttonStyles = [styles.button];
+  props.fluid && buttonStyles.push(styles.fluid);
+  props.primary && buttonStyles.push(styles.primary);
+  props.secondary && buttonStyles.push(styles.secondary);
+  props.disabled && buttonStyles.push(styles.disabled);
+
+  if (props.href) {
+    buttonStyles.push(styles.href);
+    return (
+      <Link to={props.href} className={buttonStyles.join(" ")}>
+        {props.children}
+      </Link>
+    );
+  }
+
+  if (props.type) {
+    return (
+      <button
+        className={buttonStyles.join(" ")}
+        type={props.type}
+        disabled={props.disabled}
+      >
+        {props.children}
+      </button>
+    );
+  }
+
+  return (
+    <button className={buttonStyles.join(" ")} disabled={props.disabled}>
+      {props.children}
+    </button>
+  );
+};
+export default Button;
