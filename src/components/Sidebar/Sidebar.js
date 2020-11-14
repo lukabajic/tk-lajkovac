2ac8c96bc7f4df8c19814fb6726bb12331fb8dc8
lukabajic <lukabajic.dev@gmail.com>
2020-11-14 12:05:34 +0100

    Simplify Sidebar component

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,23 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { connect } from "react-redux";
 
 import Nav from "../Nav/Nav";
-// import Backdrop from "../Utility/Backdrop/Backdrop";
 
-import { sidebarToggle } from "../../store/actions";
-
-const Sidebar = ({ sidebarToggle, active }) => {
-  const sidebar = (
-    <React.Fragment>
-      {/* {active ? <Backdrop clicked={sidebarToggle} /> : null} */}
-      <Nav />
-    </React.Fragment>
-  );
-
-  return ReactDOM.createPortal(sidebar, document.getElementById("sidebar"));
+const Sidebar = () => {
+  return ReactDOM.createPortal(<Nav />, document.getElementById("sidebar"));
 };
 
-const mapStateToProps = (state) => ({ active: state.sidebar.active });
-
-export default connect(mapStateToProps, { sidebarToggle })(Sidebar);
+export default Sidebar;
